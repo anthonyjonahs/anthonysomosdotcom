c300ffa824133ff316d478a9c998cd46a56aebc2
Anthony Somos <anthony.somos@gmail.com>
2017-07-18 21:04:10 -0400

    Check env for PORT

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const pug = require('pug');
 const routes = require('./routes.js')
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
 app.use('/', routes)
 app.use(express.static(__dirname))
-app.listen(3000, () => {
-	console.log('Listening on 3000');
+app.listen(PORT, () => {
+	console.log(`Listening on ${PORT}`);
 })
